40a22ca24f44fa43955b85bb28124e0a4925e326
tejasmankar <tejasmankar18@gmail.com>
2020-03-22 20:40:59 +0530

    Added the functionality to read the URLs to be opened directly from a file
    The Websites_List.txt file and the OpenDefaultTabsInBrowser.py file should be saved in the same location

--- OpenDefaultTabsInBrowser.py
+++ OpenDefaultTabsInBrowser.py
@@ -3,8 +3,7 @@ import webbrowser
 #Enter the name of the browser to open
 browser_name = 'chrome'
 
-#Enter the URLs of websites which should be opened by default
-website_url_list = ['www.google.com','https://www.geeksforgeeks.org/','https://leetcode.com/']
+#Enter the URLs of the websites which should be opened by default in the file 'Websites_List.txt' 
 
 #Enter the path containing the .exe file of the selected browser
 browser_path = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
@@ -12,6 +11,7 @@ browser_path = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
 #Register the web browser
 webbrowser.register('chrome', None, webbrowser.BackgroundBrowser(browser_path))
 
-#Open the selected websites in separate tabs in the browser
-for url in website_url_list:
-    webbrowser.get(browser_name).open_new_tab(url)
+#Open the URLs read from the input file in separate tabs in the selected browser
+with open('Websites_List.txt') as file:
+    for url in file:
+        webbrowser.get(browser_name).open_new_tab(url)
